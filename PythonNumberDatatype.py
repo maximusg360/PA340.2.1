cbf44767ead5c00b5f7282bd2be2f20eeb2a9348
Guzman <francisco.guzman@accenture.com>
2024-10-11 16:03:14 -0400

    PA340.2.1

---
+++ PythonNumberDatatype.py
@@ -0,0 +1,72 @@
+# Declare two integer variables
+num1 = 10
+num2 = 25
+
+# Add them together and assign the result to a variable
+sum_result = num1 + num2
+
+# Print out the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+# Declare two float variables
+num1 = 10.5
+num2 = 25.3
+
+# Add them together and assign the result to a variable
+sum_result = num1 + num2
+
+# Print out the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+# Declare an integer variable and a float variable
+num1 = 7       # Integer
+num2 = 14.5    # Float
+
+# Add them together and assign the result to a variable
+sum_result = num1 + num2
+
+# Print out the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+# The variable type of the sum is a float.
+
+# Declaring two integer variables
+num1 = 20
+num2 = 5
+
+# Dividing the larger number by the smaller number
+if num1 > num2:
+    result_integer_division = num1 / num2
+else:
+    result_integer_division = num2 / num1
+
+# Printing the result
+print("Result of integer division:", result_integer_division)
+
+# Product prices
+coffee_price = 2.50
+cappuccino_price = 3.75
+espresso_price = 2.00
+
+# Calculating subtotal for the order
+subtotal = (3 * coffee_price) + (4 * cappuccino_price) + (2 * espresso_price)
+
+# Sales tax
+SALES_TAX = 0.08
+totalSale = subtotal + (subtotal * SALES_TAX)
+
+# Printing the results
+print("Subtotal:", subtotal)
+print("Total Sale (with tax):", totalSale)
+
+# Radius of the circle
+radius = 63
+
+# Calculating the area of the circle
+pi = 3.14
+area = pi * (radius ** 2)
+
+# Printing the result
+print("Area of the circle:", area)
+
+
